efb7c52e1b0ee2a0454c7b22dc080f1990f36587
Ivan <iqsk81ad@gmail.com>
2017-01-10 16:34:49 +0200

    #

--- src/components/PropTypesList/PropTypesList.js
+++ src/components/PropTypesList/PropTypesList.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
 
 export const styles = {
   list: {
@@ -13,13 +15,45 @@ export const styles = {
   },
 };
 
-export default ({ propTypes, ...rest }) => (
-  <div style={styles.list} {...rest}>
-    <div style={styles.title}>Props:</div>
-    {
-      Object.keys(propTypes).map(key => <div key={key} style={styles.item}>{key}: {JSON.stringify(propTypes[key])}</div>)
-    }
-  </div>
-);
+export default class PropTypesList extends Component {
 
+  state = {
+    isOpen: false,
+  };
 
+  handleClose = () => this.setState({ isOpen: false });
+
+  handleOpen = () => this.setState({ isOpen: true });
+
+  render() {
+
+    const { propTypes, ...rest } = this.props;
+
+    return (
+      <div style={styles.general} {...rest}>
+        <RaisedButton label="Show all propTypes" onTouchTap={this.handleOpen} />
+        <Dialog
+          title="Dialog With Actions"
+          actions={[
+            <FlatButton
+              label="Cancel"
+              primary={true}
+              onTouchTap={this.handleClose}
+            />
+          ]}
+          modal
+          open={this.state.isOpen}
+        >
+          <div style={styles.list}>
+            <div style={styles.title}>Props:</div>
+            {
+              Object.keys(propTypes).map(key => <div key={key} style={styles.item}>{key}: {JSON.stringify(propTypes[key])}</div>)
+            }
+          </div>
+        </Dialog>
+      </div>
+    );
+
+  }
+
+}
