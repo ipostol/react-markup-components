5625ca52c7a142f110f69d1555d3773985acdd35
Ivan <iqsk81ad@gmail.com>
2017-01-10 17:13:33 +0200

    #

--- src/components/PropsChange/PropsChange.js
+++ src/components/PropsChange/PropsChange.js
@@ -1,4 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 export default class PropsChange extends Component {
 
@@ -8,9 +12,49 @@ export default class PropsChange extends Component {
     onPropsChange: PropTypes.func,
   };
 
+  checkType(field) {
+
+    const { propTypes, onPropsChange, props } = this.props;
+    const type = propTypes[field];
+    const value = props[field];
+
+    if (Array.isArray(type)) {
+
+      return (
+        <SelectField onChange={(e, number, value) => onPropsChange(field, value)}>
+          {
+            value.map(item => <MenuItem value={item} primaryText={item} />)
+          }
+        </SelectField>
+      );
+
+    } else if (type === boolean) {
+
+      return (
+        <Toggle
+          label={field}
+          toggled={!!value}
+          onToggle={() => onPropsChange(field, !value)}
+        />  
+      );
+
+    } else {
+
+      return (
+        <TextField
+          hintText={field}
+          value={value},
+          onChange={(e, value) => onPropsChange(field, value)}
+        />
+      );
+
+    }
+
+  }
+
   render() {
 
-    const { propTypes, props, onPropsChange } = this.props;
+    const { propTypes, props } = this.props;
 
     return (
       <div>
