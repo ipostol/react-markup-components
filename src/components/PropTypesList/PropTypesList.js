34f17c51f2ff7d96196e6589c98b1048a9b42392
Ivan <iqsk81ad@gmail.com>
2017-01-10 16:23:04 +0200

    #

--- src/components/PropTypesList/PropTypesList.js
+++ src/components/PropTypesList/PropTypesList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export const styles = {
+  title: {},
   list: {
 
   },
@@ -11,8 +12,9 @@ export const styles = {
 
 export default ({ propTypes, ...rest }) => (
   <div style={styles.list} {...rest}>
+    <div style={styles.title}>Props:</div>
     {
-      Object.keys(propTypes).map(key => <div style={styles.item}>{key}: {JSON.stringify(propTypes[key])}</div>)
+      Object.keys(propTypes).map(key => <div key={key} style={styles.item}>{key}: {JSON.stringify(propTypes[key])}</div>)
     }
   </div>
 );
